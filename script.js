07561dd26c294c692ea1eb71721c08a94bcc0e20
mryaxyobek <pubgn9642@gmail.com>
2023-06-03 13:27:07 +0500

    adde next js codes

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const elHamburgerBtn = document.querySelector('.hamburger-btn')
 const elExitBtn = document.querySelector('.exit-btn')
 const elNavbarMenu = document.querySelector('.mobile-navbar-menu')
-
 elHamburgerBtn.addEventListener('click', function(){
     elNavbarMenu.classList.toggle('display-block')
     
 })
 elExitBtn.addEventListener('click', function(){
     elNavbarMenu.classList.toggle('display-block')
+})
+
+// night mode 
+const elNightModeBtn = document.querySelector('.night-mode-btn')
+
+elNightModeBtn.addEventListener('click', function(){
+    document.body.classList.toggle('night-mode')
+})
+// mobile night mode 
+const elMobileNightModeBtn = document.querySelector('.mobile-night-mode-btn')
+
+elMobileNightModeBtn.addEventListener('click', function(){
+    document.body.classList.toggle('night-mode')
 })
