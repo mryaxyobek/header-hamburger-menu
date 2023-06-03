632edcabccd22adc068ee07dd7aeeb7de3cc04c8
mryaxyobek <pubgn9642@gmail.com>
2023-06-03 14:18:17 +0500

    added blur filter

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const elHamburgerBtn = document.querySelector('.hamburger-btn')
-const elExitBtn = document.querySelector('.exit-btn')
-const elNavbarMenu = document.querySelector('.mobile-navbar-menu')
+const elHamburgerBtn = document.querySelector('.hamburger-btn');
+const elExitBtn = document.querySelector('.exit-btn');
+const elNavbarMenu = document.querySelector('.mobile-navbar-menu');
+const elBlurFilter = document.querySelector('.blur-filter');
+
+// Hamburger menu
 elHamburgerBtn.addEventListener('click', function () {
     elNavbarMenu.classList.toggle('display-block')
-
+    elBlurFilter.classList.toggle('blur-filter-active')
 })
+// Exit 
 elExitBtn.addEventListener('click', function () {
     elNavbarMenu.classList.toggle('display-block')
+    elBlurFilter.classList.toggle('blur-filter-active')
 })
-
+// remove blur and exit
+elBlurFilter.addEventListener('click', function () {
+    elBlurFilter.classList.toggle('blur-filter-active')
+    elNavbarMenu.classList.toggle('display-block')
+})
 // night mode 
-const elNightModeBtn = document.querySelector('.night-mode-btn')
+const elNightModeBtn = document.querySelector('.night-mode-btn');
 
 elNightModeBtn.addEventListener('click', function () {
     document.body.classList.toggle('night-mode')
 })
 // mobile night mode 
-const elMobileNightModeBtn = document.querySelector('.mobile-night-mode-btn')
+const elMobileNightModeBtn = document.querySelector('.mobile-night-mode-btn');
 
 elMobileNightModeBtn.addEventListener('click', function () {
     document.body.classList.toggle('night-mode')
